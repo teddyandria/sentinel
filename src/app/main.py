8e2341de4afd0d4fe38d4bad38d909d49c03160f
Teddy Andriamam <te2d@Mac.lan>
2026-05-02 19:19:21 +0200

    news api fonctionnel

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+import requests
+from datetime import date, timedelta
+from .config import settings
+
+
 
 app = FastAPI()
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    from_date = (date.today() - timedelta(days=7)).isoformat()
+    url = (
+        f"https://newsapi.org/v2/everything?"
+        f"q=Apple&"
+        f"from={from_date}&"
+        f"sortBy=popularity&"
+        f"apiKey={settings.news_api_key}"
+    )
+    response = requests.get(url)
+    return response.json()
